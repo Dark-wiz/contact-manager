556bc500691397c10be5b9e38ea45a7df5fee4a8
Aronu Somtochukwu <somatic20@yahoo.com>
2019-01-21 09:45:50 +0100

    added properties to Contact and Header type checking with propTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>App Component</h1>
-        <Contact />
+        <Header />
+        <Contact name="john" email="john@mail.com" phone="0908742" />
       </div>
     );
   }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+
 class Contact extends Component {
   state = {};
   render() {
+    const { name, phone, email } = this.props;
     return (
       <div>
-        <h4>
-          <ul>
-            <li>Email: admin@mail.com</li>
-            <li>Phone: 08034456333</li>
-          </ul>
-        </h4>
+        <h4>{name} </h4>
+        <ul>
+          <li>Email: {email}</li>
+          <li>Phone: {phone}</li>
+        </ul>
       </div>
     );
   }
 }
+
+Contact.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired
+};
 
 export default Contact;
