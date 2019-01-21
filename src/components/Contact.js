89c65f45003499e6c0f484d7696fa4696d046696
Aronu Somtochukwu <somatic20@yahoo.com>
2019-01-21 07:54:12 +0100

    create new react app Add contact component

---
+++ src/components/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+class Contact extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h4>
+          <ul>
+            <li>Email: admin@mail.com</li>
+            <li>Phone: 08034456333</li>
+          </ul>
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default Contact;
