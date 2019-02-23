034af7bd657a0a2dc3706c7cec6d75fa017b5de3
Aronu Somtochukwu <somatic20@yahoo.com>
2019-02-23 15:32:52 +0100

    Updates

---
+++ precache-manifest.1411f93b3b4b32374fae1dc9a51d33e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "5cc7dc5b8a150525f826",
+    "url": "/contact-manager/static/js/main.5cc7dc5b.chunk.js"
+  },
+  {
+    "revision": "d48195402d13f9697d79",
+    "url": "/contact-manager/static/js/2.d4819540.chunk.js"
+  },
+  {
+    "revision": "5cc7dc5b8a150525f826",
+    "url": "/contact-manager/static/css/main.21809cfa.chunk.css"
+  },
+  {
+    "revision": "d48195402d13f9697d79",
+    "url": "/contact-manager/static/css/2.28ce1e2d.chunk.css"
+  },
+  {
+    "revision": "7e6ff848bf4a7692a461d2c7e24dbbb1",
+    "url": "/contact-manager/index.html"
+  }
+];
